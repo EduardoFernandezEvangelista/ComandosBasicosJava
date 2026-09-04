ffe3bff62006ebb4736f3292a42010ce8bbacdc5
EduardoFernandezEvangelista <eduardofernandezevangelista100@hotmail.com>
2026-09-04 16:50:18 -0300

    Add user input handling for name and age

--- Entrada_Saida/EntradaSaidaJava.java
+++ Entrada_Saida/EntradaSaidaJava.java
@@ -1,15 +1,19 @@
+//importamos o Scanner
 import java.util.Scanner;
 
 public class EntradaSaidaJava {
 
     public static void main(String[] args) {
 
+        // Declaramos o Scanner como função "scanner"
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite seu nome: ");
+        // A variavel nome recebe o nome do usuario
         String nome = scanner.nextLine();
 
         System.out.print("Digite sua idade: ");
+        // A variavel idade recebe a idade do usuario
         int idade = scanner.nextInt();
 
         if (idade < 0) {
@@ -19,6 +23,7 @@ public class EntradaSaidaJava {
             System.out.println("Você tem " + idade + " anos.");
         }
 
+        // Paramos de usar o scanner e fechamos ele
         scanner.close();
     }
 }
